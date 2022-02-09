d31eaa1f356a05589daff7799788b0a5683f2bec
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-09 12:19:11 +0530

    day 6

--- shopapp/src/components/Product.tsx
+++ shopapp/src/components/Product.tsx
@@ -1,5 +1,21 @@
-const Product = () => {
-	return <h1>Product!!!</h1>
+import { Link } from "react-router-dom";
+import IProduct from "../model/IProduct";
+import { Button } from "./Button";
+
+const Product:React.FC<IProduct> = (product) => {
+	let {id,name, image} = product;
+	return (
+		<div className="col-md-4 col-lg-3 my-2">
+			<div className="card">
+				<div className="img-container">
+					<Link to={`/details/${id}`} className="link">
+						<img src={image} alt={name} style={{'width':'100%', height:'200px'}}/>
+					</Link>
+					<Button className="cart-btn fa fa-cart-plus"/>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Product;
--- shopapp/src/components/ProductList.tsx
+++ shopapp/src/components/ProductList.tsx
@@ -1,5 +1,16 @@
+import {useContext} from 'react';
+import { ProductContext } from '../context/ProductContext';
+import Product from './Product';
+
 const ProductList = () => {
-	return <h1>ProductList!!!</h1>
+	const  {products} = useContext(ProductContext);
+	return <div className="container">
+		<div className="row">
+			{
+				products.map( product => <Product key={product.id} {...product}/>)
+			}
+		</div>
+	</div>
 }
 
 export default ProductList;
