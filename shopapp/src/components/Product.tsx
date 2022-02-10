749d32f50824578fa30b4af31be05988e3404231
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-10 10:36:14 +0530

    day 7

--- shopapp/src/components/Product.tsx
+++ shopapp/src/components/Product.tsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 import { ProductContext } from "../context/ProductContext";
 import IProduct from "../model/IProduct";
 import { Button } from "./Button";
+import Cart from "./Cart";
 
 const Product:React.FC<IProduct> = (product) => {
 	let {id,name, image} = product;
 	const {handleDetail} = useContext(ProductContext);
+	const {addToCart} = useContext(CartContext);
 	return (
 		<div className="col-md-4 col-lg-3 my-2">
 			<div className="card">
@@ -14,7 +17,7 @@ const Product:React.FC<IProduct> = (product) => {
 					<Link to={`/details/${id}`} className="link" onClick={() => handleDetail(id)}>
 						<img src={image} alt={name} style={{'width':'100%', height:'200px'}}/>
 					</Link>
-					<Button className="cart-btn fa fa-cart-plus"/>
+					<Button className="cart-btn fa fa-cart-plus" onClick={() => addToCart(id)}/>
 				</div>
 			</div>
 		</div>
---
+++ shopapp/src/context/CartContext.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import cartReducer, { ICart, StateType } from "../reducer/cart_reducer";
+import { ProductContext } from "./ProductContext";
+
+// export const CartContext = React.createContext<{cart:ICart[], 
+//     addToCart:(id:string) => void,
+//     increment:(id:string) => void}>({
+//     cart:[],
+//     addToCart:(id:string) => {},
+//     increment:(id:string) => {} 
+// });
+
+export const CartContext = React.createContext();
+const initialState = {
+    products:[],
+    total:0
+}
+
+export const CartProvider = (props) => {
+    let [state, dispatch] = React.useReducer(cartReducer, initialState);
+    let {products}  = React.useContext(ProductContext);
+    function addToCart(id) {
+        let prd =products.filter(p => p.id === id) [0];
+        dispatch({type:'ADD_TO_CART', payload:prd})
+    }
+    function increment(id) {
+        dispatch({type:'INCREMENT', payload:id})
+    }
+    return <CartContext.Provider value={{cart: state.products, addToCart: addToCart, increment: increment}}>
+        {props.children}
+    </CartContext.Provider>
+}
