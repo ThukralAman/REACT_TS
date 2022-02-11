2481ce80c0a8e836ebb56b7557a6c494bc7c8c8e
Banuprakash <banuprakashc@yahoo.co.in>
2022-02-11 10:36:15 +0530

    day 8

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import {createContact, deleteContact} from './redux/ActionCreators'
+import { connect } from 'react-redux';
+import ContactView from './ContactView';
+
 class App extends React.Component {
   nameRef = React.createRef();
   emailRef = React.createRef();
+  
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
   
   handleSubmit(e) {
     e.preventDefault();
@@ -12,8 +21,12 @@ class App extends React.Component {
     }
     this.nameRef.current.value = "";
     this.emailRef.current.value = "";
+    this.props.createNewContact(contact);
   }
   
+  deleteContact(email) {
+    this.props.deleteAContact(email);
+  }
   render() {
     <div>
       <h1>Contacts Application</h1>
@@ -22,6 +35,26 @@ class App extends React.Component {
         Email: <input type="text" ref={this.emailRef}/> <br />
         <button type="submit">Submit</button>
       </form>
+      {
+        this.props.contacts.map( contact => <ContactView 
+          key={contact.email} contact={contact} deleteContact={ (email) => this.deleteContact(email)}/>)
+      }
     </div>
   }
 }
+// state from "redux store"
+// return props
+const mapStateToProps = (state) => {
+  return {
+    contacts: state.contacts
+  }
+} 
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createNewContact : contact => dispatch(createContact(contact)),
+    deleteAContact: email => dispatch(deleteContact(email))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
